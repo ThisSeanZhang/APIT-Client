387a8e4fc18b4cf4838373825e00fb9fbb7bc235
ThisSeanZhang <thisseanzhang@gmail.com>
2019-02-14 11:30:02 +0800

    更改本地调试的端口和远程调试的地址

--- src/renderer/config/index.js
+++ src/renderer/config/index.js
@@ -1,3 +1,3 @@
 const isPro = Object.is(process.env.NODE_ENV, 'development')
-const baseUrl = isPro ? 'http://localhost/' : 'http://test.whileaway.io/'
+const baseUrl = isPro ? 'http://localhost:8080/' : 'http://apit.whileaway.io/'
 export {baseUrl}
